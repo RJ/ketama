3caa4f1296c58f3f551f342437900a0ae05d2139
Miki Tebeka <miki@saucelabs.com>
2009-09-01 14:32:08 -0700

    Specify include directory (for OSX)

--- python_ketama/setup.py
+++ python_ketama/setup.py
@@ -1,4 +1,7 @@
 from distutils.core import setup, Extension
 
 setup(name="ketama", version="0.1",
-    ext_modules=[Extension("ketama", ["ketamamodule.c"], library_dirs=["/usr/local/lib"], libraries=["ketama"])])
+      ext_modules=[Extension("ketama", ["ketamamodule.c"],
+                             library_dirs=["/usr/local/lib"],
+                             include_dirs=["/usr/local/include"],
+                             libraries=["ketama"])])
